944ca0cb8eff856c8ef4d1bbb2d9365f73f92cef
titanexp <nagatak48@gmail.com>
2025-12-04 17:36:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const CACHE_NAME = 'gemini-pwa-cache-v1'; // キャッシュ名を変更すると強制的に更新がかかる場合がある
+const CACHE_NAME = 'gemini-pwa-cache-v2'; // キャッシュ名を変更すると強制的に更新がかかる場合があ
 const urlsToCache = [
   './', // ルートパス (index.html を指すことが多い)
   './index.html',
